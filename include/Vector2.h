e25dd12e39953c8c6d7cb1472bd1e4e9091abc31
Jack Woods <jackrwoods@gmail.com>
2017-07-12 21:52:27 -0700

    Finish Vector2 header file.

---
+++ include/Vector2.h
@@ -0,0 +1,27 @@
+#ifndef VECTOR2_CPP
+#define VECTOR2_CPP
+
+
+class Vector2 {
+	private:
+		// Store x and y coordinates
+		int x, y;
+	public:
+		
+		// Getters and setters
+		int getX();
+		int getY();
+		void setX(int);
+		void setY(int);
+		
+		// Helpful functions
+		void addToX(int); // Adds integer parameter 
+				  // to current x value
+		void addToY(int); // Adds integer parameter
+				  // to current y value
+		/* NOTE: To subtract from x or y, pass  a
+			 negative value into the function.
+}
+
+
+#endif
