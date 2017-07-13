c4722a1cf468b9a20f178eaa74b56b30c9a0106a
Jack Woods <jackrwoods@gmail.com>
2017-07-13 10:50:36 -0700

    Start Terrain class & start PlatformVector class.

---
+++ include/Terrain.hpp
@@ -0,0 +1,13 @@
+#ifndef TERRAIN_HPP
+#define TERRAIN_HPP
+
+#include "LinkedList.h"
+
+class Terrain {
+	private:
+		LinkedList* vectors;
+	public:
+		Terrain(int); // Pass in terrain size
+}
+
+#endif
