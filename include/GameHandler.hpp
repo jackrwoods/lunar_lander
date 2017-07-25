866de521119ba54c974ecf2a2e5e1651b57b44c4
Jack Woods <jackrwoods@gmail.com>
2017-07-25 00:20:55 -0700

    Change GameHandler to a game class.

--- include/GameHandler.hpp
+++ include/GameHandler.hpp
@@ -1,7 +1,23 @@
 #ifndef GAMEHANDLER_HPP
 #define GAMEHANDLER_HPP
 
+#include "SDL2/SDL.h"
+#include "Player.hpp"
+#include "Terrain.hpp"
+#include <cmath>
 
+class GameHandler {
+  private:
+    Player p;
+    Terrain t;
+  public:
+    GameHandler();
 
+    /* Change the thrust currently being applied to the player. */
+    void UpdateThrust(int, int);
+
+    /* Change the player's direction based on keyboard input. */
+    void UpdateRotation(SDL_Keysym, int);
+};
 
 #endif
--- src/GameHandler.cpp
+++ src/GameHandler.cpp
@@ -1,1 +1,16 @@
 // Physics Calculations
+
+#include "../include/GameHandler.hpp"
+
+
+void GameHandler::UpdateThrust(int y, int delta) {
+  int thrust;
+  if (y > 100) {
+    thrust = 100;
+  } else if (y < 0) {
+    thrust = 0;
+  } else {
+    thrust = y;
+  }
+  p.setThrust(thrust);
+}
